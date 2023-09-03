5ee3b7b14a48468f3ed1b9bc051e51fc0b965c60
kamegoro <tentyo0620@icloud.com>
2023-09-03 11:59:12 +0900

    add hot reload func

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Div Rhino!")
+		return c.SendString("Hello, Div Rhino!!!")
 	})
 
 	app.Listen(":3000")
