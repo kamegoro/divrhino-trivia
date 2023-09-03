07367eb308c6f46b773dc3331e406b2dfe945efb
kamegoro <tentyo0620@icloud.com>
2023-09-03 13:34:02 +0900

    fix facts model

--- models/models.go
+++ models/models.go
@@ -7,5 +7,5 @@ import (
 type Fact struct {
 	gorm.Model
 	Question string `json:"question" gorm:"text;not null;default:null"`
-	Answer string `json:"answer" gorm:"text;note null;default:null"`
+	Answer   string `json:"answer" gorm:"text;not null;default:null"`
 }
